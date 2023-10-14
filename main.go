d9d7978d79cb6953e4505b7fcc39d65e520f7450
Logan Cox <mail@logan-cox.com>
2023-10-14 13:22:00 +0100

    implement functionality to count lines in a file

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"bufio"
 )
 
 func check(e error) {
@@ -19,12 +20,39 @@ func countBytes(fileName string) int64 {
 	return fileSize
 }
 
+func countLines(fileName string) int {
+	var lineCount int
+
+	file, err := os.Open(fileName)
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lineCount ++
+	}
+	return lineCount
+}
+
 func main () {
+	var fileName string
+	
 	// Build out flags
 	bytesFlag := flag.String("c", "", "Count the bytes in a file")
+	linesFlag := flag.String("l", "", "Count the number of lines in a file")
 	flag.Parse()
-	fileName := *bytesFlag
 
-	fileBytes := countBytes(fileName)
-	fmt.Println(" ", fileBytes, fileName)
+	// Return bytes in a file
+	if len(*bytesFlag) > 0 {
+		fileName = *bytesFlag
+		fileBytes := countBytes(fileName)
+		fmt.Println(" ", fileBytes, fileName)
+	}
+
+	// Return lines in a file
+	if len(*linesFlag) > 0 {
+		fileName = *linesFlag
+		fileLines := countLines(fileName)
+		fmt.Println(" ", fileLines, fileName)
+	}
 }
