87c2ede54a269cae37b43ce0517ad15ee6d52bc3
Logan Cox <mail@logan-cox.com>
2023-12-04 10:59:12 +0000

    feat(unit-test): adding tests to cover line count

--- main.go
+++ main.go
@@ -21,11 +21,7 @@ func CountLines(bytes []byte) int {
 }
 
 func CountBytes(bytes []byte) int {
-	var byteCount int
-
-	for i := 0; i < len(bytes); i++ {
-		byteCount++
-	}
+	byteCount := len(bytes)
 
 	return byteCount
 }
